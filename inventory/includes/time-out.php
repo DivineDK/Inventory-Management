3ae83155a4672484bd19f2e1b59b3078bcee91b0
Dkhunk <80922939+DivineDK@users.noreply.github.com>
2024-06-07 00:50:47 +0000

    Inventory-Management

---
+++ inventory/includes/time-out.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+
+// Set the idle time limit in seconds (10 minutes in this example)
+$idle_limit = 10;
+
+// Check if the user is logged in
+if (!isset($_SESSION['id'])) {
+    header("Location: login.php");
+    exit();
+}
+
+// Update the last activity time
+$_SESSION['last_activity'] = time();
+
+// Check if the user has been idle for too long
+if (isset($_SESSION['last_activity']) && (time() - $_SESSION['last_activity'] > $idle_limit)) {
+    // Notify the user with JavaScript alert
+    echo '<script>alert("You have been idle for too long. You will be redirected to the login page.");</script>';
+    
+    // Redirect to the login page after a short delay
+    echo '<script>setTimeout(function(){ window.location.href = "login.php"; }, 2000);</script>';
+    exit();
+}
+
+// Your regular page content goes here
+
+?>
